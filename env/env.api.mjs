e1f24edc4f11e18ec702721e0601dd41041dc0bc
anhchangvt1994 <anhchangvt1994@gmail.com>
2024-09-04 02:11:36 +0700

    update(comp-page): :jigsaw: update
    update api config

--- env/env.api.mjs
+++ env/env.api.mjs
@@ -4,7 +4,8 @@ export default {
 		base:
 			process.env.MODE === 'production'
 				? // ? 'https://on-ferret-above.ngrok-free.app/api'
-				  'https://spicy-lion-10.telebit.io/api'
+				  // 'https://spicy-lion-10.telebit.io/api'
+				  'http://154.205.142.138:8080/api'
 				: 'http://localhost:8080/api',
 		page_speed: {
 			get_info: {
