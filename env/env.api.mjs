ddc208ca524355933c3e29d235e64d28fab43c3d
anhchangvt1994 <anhchangvt1994@gmail.com>
2024-09-04 11:30:49 +0700

    update(other): :jigsaw: update
    update env.api.mjs

--- env/env.api.mjs
+++ env/env.api.mjs
@@ -5,7 +5,7 @@ export default {
 			process.env.MODE === 'production'
 				? // ? 'https://on-ferret-above.ngrok-free.app/api'
 				  // 'https://spicy-lion-10.telebit.io/api'
-				  'http://154.205.142.138:8080/api'
+				  'https://chilly-swan-3.telebit.io/api'
 				: 'http://localhost:8080/api',
 		page_speed: {
 			get_info: {
