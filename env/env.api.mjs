8cdff48bc95a5aea29aa33ad5cad46b07fbf1178
anhchangvt1994 <anhchangvt1994@gmail.com>
2024-09-05 23:49:50 +0700

    update(other): :jigsaw: update
    update api base url

--- env/env.api.mjs
+++ env/env.api.mjs
@@ -3,9 +3,9 @@ export default {
 	data: {
 		base:
 			process.env.MODE === 'production'
-				? // ? 'https://on-ferret-above.ngrok-free.app/api'
+				? // 'https://on-ferret-above.ngrok-free.app/api'
 				  // 'https://spicy-lion-10.telebit.io/api'
-				  'https://chilly-swan-3.telebit.io/api'
+				  'https://anhchangvt1994.site/api'
 				: 'http://localhost:8080/api',
 		page_speed: {
 			get_info: {
